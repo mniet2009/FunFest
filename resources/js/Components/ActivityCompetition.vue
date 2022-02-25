2a76643f24148f96d856a67e93c48fdebfc2ad8d
Maurice <maurice@guegan.de>
2022-02-25 12:12:02 +0100

    design stuff?

--- resources/js/Components/ActivityCompetition.vue
+++ resources/js/Components/ActivityCompetition.vue
@@ -5,35 +5,39 @@
       :endDate="this.activity.event_at"
     ></Countdown>
 
-    <v-simple-table v-else>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Player</th>
-          <th>Tickets</th>
-        </tr>
-      </thead>
-      <tbody>
-        <router-link
-          as="tr"
-          v-for="(completion, i) in activity.completions"
-          :key="i"
-          :href="route('users.show', completion.user)"
-          class="pointer"
-          v-ripple
-        >
-          <td>{{ completion.placement }}</td>
-          <td>
-            <user-avatar
-              :url="completion.user.id"
-              :username="completion.user.username"
-              :color="$page.props.teams[completion.user.team_id - 1].color"
-            ></user-avatar>
-          </td>
-          <td>{{ completion.tickets }}</td>
-        </router-link>
-      </tbody>
-    </v-simple-table>
+    <v-card v-else>
+      <v-card-title>Leaderboard</v-card-title>
+
+      <v-simple-table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Player</th>
+            <th>Tickets</th>
+          </tr>
+        </thead>
+        <tbody>
+          <router-link
+            as="tr"
+            v-for="(completion, i) in activity.completions"
+            :key="i"
+            :href="route('users.show', completion.user)"
+            class="pointer"
+            v-ripple
+          >
+            <td>{{ completion.placement }}</td>
+            <td>
+              <user-avatar
+                :url="completion.user.id"
+                :username="completion.user.username"
+                :color="$page.props.teams[completion.user.team_id - 1].color"
+              ></user-avatar>
+            </td>
+            <td>{{ completion.tickets }}</td>
+          </router-link>
+        </tbody>
+      </v-simple-table>
+    </v-card>
   </div>
 </template>
 
